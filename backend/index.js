9cb14dbe1b015ca20ff6d5b78fe8406379665771
Maudhio Andre <maudhioa@gmail.com>
2024-11-15 14:53:54 +0700

    .

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ mongoose.connect(`${process.env.DB_LINK}`,{
 
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("Database Connected..."));
+db.once("open", () => console.log("Database Connected....."));
 
 app.use(cors());
 app.use(express.json());
